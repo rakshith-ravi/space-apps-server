4a09740a38bbe5bbd443ccfa60acd19e2e86c767
Rakshith Ravi <rakshithravi1997@gmail.com>
2017-04-29 22:03:31 +0530

    Login and signup done with JWT

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var mysql = require('mysql');
 var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+var guid = require('guid');
 var router = express.Router();
 
 const saltRounds = 10;
+const secret = "don't tell anyone";
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -14,12 +17,20 @@ var connection = mysql.createConnection({
 connection.connect();
 
 router.post('/login', function(req, res) {
-  connection.query('SELECT * FROM users WHERE username = ?;', function(error, results, fields) {
+  connection.query('SELECT * FROM users WHERE username = ?;', [req.body.username], function(error, results, fields) {
     if(error) {
       console.error(error);
       res.json({
         success: false,
+        error: "errordb",
         message: "Error checking database"
+      });
+    }
+    if(results.length == 0) {
+      res.json({
+        success: false,
+        error: "invaliduser",
+        message: "User not found"
       });
     }
     bcrypt.compare(req.body.password, results[0].password, function(err, answer) {
@@ -27,18 +38,67 @@ router.post('/login', function(req, res) {
         console.error(err);
         res.json({
           success: false,
+          error: "errorpassword",
           message: "Error checking password"
         });
       }
       if(answer) {
-        
+        jwt.sign(req.body.username, secret, function(err, token) {
+          res.json({
+            success: true,
+            accesstoken: token
+          });
+        });
+      } else {
+        res.json({
+          success: false,
+          error: "invalidpassword",
+          message: "Invalid password"
+        });
       }
     })
   });
 });
 
-router.get('/', function(req, res) {
-  res.render('index');
+router.post('/signup', function(req, res) {
+  connection.query('SELECT * FROM users WHERE username = ?;', [req.body.username], function(error, results, fields) {
+    if(error) {
+      console.error(error);
+      res.json({
+        success: false,
+        error: "errordb",
+        message: "Error checking database"
+      });
+      return;
+    }
+    if(results.length > 0) {
+      res.json({
+        success: false,
+        error: "existingusername",
+        message: "That username is taken"
+      });
+      return;
+    }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      connection.query("INSERT INTO users (?, ?, ?, ?, ?, ?)", [guid.create(), req.body.username, hash, req.body.name, req.body.email, 0], function(error, results, fields) {
+        if(error) {
+          console.error(error);
+          res.json({
+            success: false,
+            error: "errordb",
+            message: "Error checking database"
+          });
+          return;
+        }
+        jwt.sign(req.body.username, secret, function(err, token) {
+          res.json({
+            success: true,
+            accesstoken: token
+          });
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
